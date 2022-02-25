f8f4921adb843c303af184a236986ab8c00aa98f
sunixzs <marcel.briefs@lbrmedia.de>
2022-02-25 17:25:29 +0100

    basic templating

---
+++ Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,10 @@
+<?php
+
+declare(strict_types=1);
+
+// add TypoScript
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'bootstrap_example',
+    'Configuration/TypoScript',
+    'General'
+);
